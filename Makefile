725efe3b4da08fd45feb72d7c85e3abbfebaa508
Lee Roland <glroland@hotmail.com>
2025-04-25 15:05:24 -0400

    run and config

---
+++ Makefile
@@ -0,0 +1,10 @@
+LLAMA_STACK_HOST := envision
+LLAMA_STACK_PORT := 8321
+LLAMA_STACK_MODEL := meta-llama/Llama-3.2-11B-Vision-Instruct
+
+install:
+	pip install -r requirements.txt
+
+run:
+	cd simple && LLAMA_STACK_HOST=$(LLAMA_STACK_HOST) LLAMA_STACK_PORT=$(LLAMA_STACK_PORT) LLAMA_STACK_MODEL=$(LLAMA_STACK_MODEL) python one_shot_inference.py
+	
--- bin/run_llama_stack.bat
+++ bin/run_llama_stack.bat
@@ -1,17 +1,9 @@
 @echo off
 
 set LLAMA_PATH=C:\Users\glrol\.llama
-set DISTRO=llamastack/distribution-meta-reference-gpu:latest
-set LLAMA_STACK_PORT=5001
-rem set LLAMA_MODEL=meta-llama/Llama-3.1-8B-Instruct
+set LLAMA_STACK_PORT=8321
 set LLAMA_MODEL=meta-llama/Llama-3.2-11B-Vision-Instruct
 
-docker pull %DISTRO%
+echo llama stack build --template meta-reference-gpu --image-type conda
 
-docker run --rm --gpus all -d -p %LLAMA_STACK_PORT%:%LLAMA_STACK_PORT% -v %LLAMA_PATH%:/root/.llama %DISTRO% --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=%LLAMA_MODEL%
-
-rem docker run --gpus all -it -p %LLAMA_STACK_PORT%:%LLAMA_STACK_PORT% -v %LLAMA_PATH%:/root/.llama %DISTRO% --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=%LLAMA_MODEL%
-
-rem docker run -it -p %LLAMA_STACK_PORT%:%LLAMA_STACK_PORT% -v %LLAMA_PATH%:/root/.llama %DISTRO% --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=meta-llama/Llama3.1-8B-Instruct --env SAFETY_MODEL=meta-llama/Llama-Guard-3-8B
-
-
+echo llama stack run distributions/meta-reference-gpu/run.yaml --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=%LLAMA_MODEL%
