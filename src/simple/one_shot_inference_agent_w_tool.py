9ff4cb2cbc3e326a381ad8f6cbac991f634ed684
Lee Roland <glroland@hotmail.com>
2025-04-27 13:19:52 -0400

    working tools (optional usage)

--- src/simple/one_shot_inference_agent_w_tool.py
+++ src/simple/one_shot_inference_agent_w_tool.py
@@ -1,6 +1,7 @@
 import os
 from llama_stack_client import LlamaStackClient, LlamaStackClient, Agent, AgentEventLogger
 from llama_stack_client.lib.agents.client_tool import client_tool
+from llama_stack_client.types.shared_params.agent_config import ToolConfig
 
 ENV_LLAMA_STACK_HOST = "LLAMA_STACK_HOST"
 ENV_LLAMA_STACK_PORT = "LLAMA_STACK_PORT"
@@ -66,7 +67,8 @@ def main():
         llama_stack_client,
         model=llama_stack_model.identifier,
         instructions="You are a helpful assistant that can use tools to answer questions.",
-        tools=[calculator, "builtin::code_interpreter"],
+        tools=[calculator],
+        tool_config=ToolConfig(tool_choice = "auto"),
     )
 
     # Create a session
@@ -75,13 +77,15 @@ def main():
     user_prompts = [
         "What are the capitals of each major european country?",
         "hello world, write me a 2 sentence poem about the moon",
-        "What is 9 * 7?"
+        "What is 9 * 7?",
         "What is 2 * 2 * 2?"
     ]
 
     # send chat completion request
     print ("Sending agent requests...")
     for prompt in user_prompts:
+        # Display Prompt
+        print ("user> ", prompt)
     
         # Send request
         response = agent.create_turn(
@@ -89,6 +93,8 @@ def main():
             messages=[{"role":"user", "content":prompt}],
             stream=True
         )
+
+        # Display Response (streaming)
         for log in AgentEventLogger().log(response):
             log.print()
 
