393c88f39344d27d03b4ca8724c47d90366a0b6e
Lee Roland <glroland@hotmail.com>
2025-04-30 17:27:17 -0400

    don't want it to push to registry every time, at least right now

--- stack/build.sh
+++ stack/build.sh
@@ -4,4 +4,4 @@ llama stack build --config build.yaml --image-type container --image-name my-lla
 
 docker tag my-llama-stack:0.2.4 registry.home.glroland.com/llama/stack:1
 
-docker push registry.home.glroland.com/llama/stack:1
+# docker push registry.home.glroland.com/llama/stack:1
