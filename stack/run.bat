8ab2bb3ff0e59af8e5f541fb5576d411eafee9f6
Lee Roland <glroland@hotmail.com>
2025-04-30 17:11:43 -0400

    tweaks before i rebuild the distro

--- stack/run.bat
+++ stack/run.bat
@@ -3,13 +3,13 @@
 set LLAMA_PATH=C:\Users\glrol\.llama
 set DISTRO=registry.home.glroland.com/llama/stack:1
 set LLAMA_STACK_PORT=8321
-rem set LLAMA_MODEL=meta-llama/Llama-3.1-8B-Instruct
 set LLAMA_MODEL=meta-llama/Llama-3.2-11B-Vision-Instruct
+set EMBEDDING_MODEL=sentence-transformers/all-mpnet-base-v2
 
 rem docker pull %DISTRO%
 
 rem docker run --rm --gpus all -d -p %LLAMA_STACK_PORT%:%LLAMA_STACK_PORT% -v %LLAMA_PATH%:/root/.llama %DISTRO% --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=%LLAMA_MODEL%
-docker run --gpus all -d -p %LLAMA_STACK_PORT%:%LLAMA_STACK_PORT% -v %LLAMA_PATH%:/root/.llama %DISTRO% --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=%LLAMA_MODEL% --env LLAMA_STACK_LOGGING=server=debug;core=info --env MILVUS_ENDPOINT=http://db.home.glroland.com:19530 --env MILVUS_TOKEN=root:Milvus
+docker run --gpus all -d -p %LLAMA_STACK_PORT%:%LLAMA_STACK_PORT% -v %LLAMA_PATH%:/root/.llama %DISTRO% --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=%LLAMA_MODEL% --env EMBEDDING_MODEL=%EMBEDDING_MODEL% --env INFERENCE_CHECKPOINT_DIR=/root/.llama/checkpoints --env MILVUS_ENDPOINT=http://db.home.glroland.com:19530 --env MILVUS_TOKEN=root:Milvus
 
 rem docker run --gpus all -it -p %LLAMA_STACK_PORT%:%LLAMA_STACK_PORT% -v %LLAMA_PATH%:/root/.llama %DISTRO% --port %LLAMA_STACK_PORT% --env INFERENCE_MODEL=%LLAMA_MODEL%
 
